2580982623824e721290fff40b31f7bace9303c9
khryss <cristian_purcaru@yahoo.com>
2015-05-29 17:40:44 +0300

    AMQP learn. Small bidirectional chat.

--- learnToUse/SQLAlchemy/SQLAlchemy.py
+++ learnToUse/SQLAlchemy/SQLAlchemy.py
@@ -10,11 +10,14 @@ class Log(Base):
     id = Column(Integer, primary_key=True)
     data = Column(String)
 
-    def __init__(self, id, data):
-    	self.id = id
-    	self.data = data
+    def __repr__(self):
+    	return "log[%s]: %s" % (self.id, self.data)
 
-log = [3]
-log[0] = Log(0, "first log")
 
-print log[0].id, log[0].data
+log = []
+log.append(Log(id='0', data="first log"))
+log.append(Log(id='1', data="second log"))
+#log[0] = Log(data="third log")
+
+for i in range(len(log)):
+	print log[i]
